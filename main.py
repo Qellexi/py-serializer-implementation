c0e63ac8d11dd75c9f938f02a2c9fc7c3b23dbe7
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2026-01-11 15:07:51 +0100

    Update serialization to use JSON encoding

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from car.serializers import CarSerializer
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return serializer.data
+    return json.dumps(serializer.data).encode("utf-8")
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    serializer = CarSerializer(data=json)
+    parsed_data = json.loads(json.decode("utf-8"))
+    serializer = CarSerializer(data=parsed_data)
     serializer.is_valid(raise_exception=True)
     return serializer.save()
