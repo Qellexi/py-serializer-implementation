7002cfa3253474ba11166cf2d9a048a71a6bf734
Yuliia Vasylieva <99039851+Qellexi@users.noreply.github.com>
2026-01-11 14:48:42 +0100

    Implement serialization and deserialization for Car

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from car.models import Car
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return serializer.data
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
